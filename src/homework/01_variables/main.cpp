07e814f6a1811b9c413670e5c3096a8be6dd4566
delaneyld <delaney.raper@g.austincc.edu>
2022-02-03 21:57:33 -0600

    HW1 with test cases

--- src/homework/01_variables/main.cpp
+++ src/homework/01_variables/main.cpp
@@ -1,13 +1,23 @@
 ﻿//write include statements
+#include<iostream>
+#include "variables.h"
 
-
-//write namespace using statement for cout
-
-
+//write namespace using statement for cout and cin
+using std::cin, std::cout;
 
 int main()
 {
-	
+	int num; 
+	cout << "Number: "; 
+	cin >> num; 
+
+	int result; 
+    result = multiply_numbers(num); 
+    cout << "The result value for " << num << " is: " << result << "\n";
+
+	int num1 = 4; 
+	result = multiply_numbers(num1); 
+	cout << "The result value for " << num1 << " is: " << result << "\n"; 
 
 	return 0;
 }
--- src/homework/01_variables/variables.cpp
+++ src/homework/01_variables/variables.cpp
@@ -6,6 +6,13 @@ int add_numbers(int num1, int num2)
 	return num1 + num2;
 }
 
+//hw function multiply numbers
+int multiply_numbers(int num1)
+{
+	int num2 = 5; 
+	return num1 * num2; 
+}
+
 
 
 
