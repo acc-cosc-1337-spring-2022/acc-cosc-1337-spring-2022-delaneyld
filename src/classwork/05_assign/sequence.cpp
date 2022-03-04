a08e6da4867621b347e8241a01dc439c2aab8e6f
delaneyld <delaney.raper@g.austincc.edu>
2022-03-03 23:50:16 -0600

    HW5 with tests and CMakeLists update. Saved under classwork/05_assign

--- src/classwork/05_assign/sequence.cpp
+++ src/classwork/05_assign/sequence.cpp
@@ -1,4 +1,183 @@
 //write include statements 
+#include <string>
+#include <iostream>
+#include <cctype>
+#include "sequence.h"
+
+using std::cout, std::string, std::cin; 
+
+//function get_gc_content 
+//returns decimal percent of G and C from a given constant dna string 
+double get_gc_content(const std::string& dna)
+{   
+    double percent_gc, size_s, count; 
+    count = 0; 
+    size_s = dna.size(); 
+
+    for (auto& ch: dna)
+    {
+
+        //convert ch to uppercase, doesn't work because ch is constant 
+        //ch = toupper(ch); 
+
+        if (ch == 'G' || ch == 'C')
+        {
+            ++count;
+        }
+
+    }
+    percent_gc = count / size_s; 
+    
+    return percent_gc; 
+
+
+}
+
+//function get_dna_complement 
+//returns the reverse complement from a given dna string value
+//calls reverse_string to get reverse dna string 
+//then finds the complement 
+string get_dna_complement(string dna)
+{
+    int count = 0; 
+
+    string dna_new = reverse_string(dna); 
+
+    for (auto ch: dna_new)
+    {
+
+        if (ch == 'A')
+        {
+            ch = 'T'; 
+            dna_new[count] = ch; 
+
+        }
+        else if (ch == 'T')
+        {
+            ch = 'A'; 
+            dna_new[count] = ch; 
+
+        }
+        else if (ch == 'G')
+        {
+            ch = 'C'; 
+            dna_new[count] = ch; 
+
+        }
+        else if (ch == 'C')
+        {
+            ch = 'G'; 
+            dna_new[count] = ch; 
+
+        }
+
+        ++count; 
+    }
+
+    return dna_new; 
+
+}
+
+//function reverse_string 
+//returns the reverse string from a given dna string value 
+string reverse_string(string dna)
+{
+    int size_s, count_for;
+    size_s = dna.size(); 
+    string new_string=dna; 
+    count_for=0; 
+
+    for(int count_back = size_s - 1; count_back >= 0; --count_back)
+    {
+        new_string[count_for] = dna[count_back]; 
+        ++count_for; 
+    }
+
+    return new_string; 
+
+}
+
+//function user_menu 
+void user_menu()
+{
+
+    cout<< "\nMAIN MENU\n\n";
+    cout<< "1 - Get GC Content\n";
+    cout<< "2 - Get DNA Complement\n";
+    cout<< "3 - Exit\n\n"; 
+
+}
+
+//function to run user menu 
+int run_user_menu()
+{
+    int choice; 
+
+    cout<<"Please enter your menu choice number: "; 
+	cin>>choice; 
+    return choice; 
+
+}
+
+//function for user menu conditions 
+void user_menu_conditions(int choice)
+{
+    string dna_test, result_string; 
+    double result_doub; 
+
+    // switch loop to handle menu choices 
+    switch(choice)
+		{
+			//GC Content
+			case 1: 
+				cout<<"Please enter a DNA string: "; 
+				cin>>dna_test; 
+				result_doub = get_gc_content(dna_test); 
+                cout<<"The decimal percent of GC is: "<< result_doub << "\n";
+				break; 
+
+			//Get DNA Complement 
+			case 2:
+				cout<<"Please enter a DNA string: "; 
+				cin>>dna_test; 
+				result_string = get_dna_complement(dna_test); 
+                cout<<"The DNA complement is: "<< result_string << "\n"; 
+				break; 
+
+			//exit with user confirm 
+			case 3:
+				break;
+
+			default:
+				cout<<"Menu number choice entered was not valid.\n"; 
+        }
+
+}
+
+//function user_confirm_check  
+char user_confirm_check()
+{
+    char user_confirm; 
+    cout << "Are you sure that you want to exit? (y/n): "; 
+    cin >> user_confirm; 
+    return user_confirm; 
+}
+
+
+
+// Helping to test code from the discussion board of this HW 
+// //function test 
+// string test_reverse(string dna)
+// {
+//     string temp_st; 
+//     for (int i = dna.length()-1; i>=0; i--)
+//     {
+//         temp_st += dna[i]; 
+
+//     }
+//     return temp_st; 
+// }
+
 
 
 /*
--- test/classwork_test/05_assign_test/sequence_tests.cpp
+++ test/classwork_test/05_assign_test/sequence_tests.cpp
@@ -1,8 +1,36 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "loops.h"
+#include "sequence.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
 
+TEST_CASE("Test function get_gc_content")
+{
+	REQUIRE(get_gc_content("AGCTATAG")==0.375); 
+	REQUIRE(get_gc_content("CGCTATAG")==0.50); 
+
+}
+
+TEST_CASE("Test function get_dna_complement")
+{
+	REQUIRE(get_dna_complement("AAAACCCGGT")=="ACCGGGTTTT"); 
+	REQUIRE(get_dna_complement("CCCGGAAAAT")=="ATTTTCCGGG"); 
+
+}
+
+TEST_CASE("Test reverse_string")
+{
+	REQUIRE(reverse_string("AGCTATAG")=="GATATCGA"); 
+	REQUIRE(reverse_string("CGCTATAG")=="GATATCGC");
+
+}
+
+// Helping to test code from the discussion board for this HW 
+// TEST_CASE("Test the sample function")
+// {
+// 	REQUIRE(test_reverse("AGCTATAG")=="GATATCGA"); 
+
+// }
